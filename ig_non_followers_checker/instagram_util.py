d9d3c17421374626f9e15c289951bc02e4b9c651
Manuel Barrera <manuel.barrera@latam.com>
2024-07-13 21:01:43 -0400

    fix test

--- ig_non_followers_checker/instagram_util.py
+++ ig_non_followers_checker/instagram_util.py
@@ -1,7 +1,7 @@
 import instaloader
 import logging
-from ig_non_followers_checker.excluded_accounts import excluded_accounts
-from ig_non_followers_checker.logging_config import setup_logging
+from excluded_accounts import excluded_accounts
+from logging_config import setup_logging
 
 setup_logging()
 
@@ -16,14 +16,12 @@ def get_non_followers(username, password):
     logging.info(f'Logged in as: {profile.username}')
     
     logging.info('Fetching followers...')
-    followers = list(profile.get_followers())
+    followers = set(profile.get_followers())
     logging.info(f'Fetched {len(followers)} followers.')
-    logging.debug(f"Followers: {[f.username for f in followers]}")
     
     logging.info('Fetching followees...')
-    followees = list(profile.get_followees())
+    followees = set(profile.get_followees())
     logging.info(f'Fetched {len(followees)} followees.')
-    logging.debug(f"Followees: {[f.username for f in followees]}")
     
     followers_usernames = {follower.username for follower in followers}
     followees_usernames = {followee.username for followee in followees}
@@ -39,6 +37,5 @@ def get_non_followers(username, password):
     non_followers -= set(excluded_accounts)
     
     logging.info(f'Identified {len(non_followers)} non-followers (excluding manually excluded accounts).')
-    logging.debug(f"Non-followers: {list(non_followers)}")
     
     return list(non_followers)
